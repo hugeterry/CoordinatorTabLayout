21cff29482bfdb26eea53790c32c3bbb965d425f
hugeterry <xuxt-314@qq.com>
2017-01-28 16:56:25 +0800

    库中基础配置完成

--- coordinatortablayout/src/main/java/cn/hugeterry/coordinatortablayout/Coordinatortablayout.java
+++ coordinatortablayout/src/main/java/cn/hugeterry/coordinatortablayout/Coordinatortablayout.java
@@ -3,8 +3,14 @@ package cn.hugeterry.coordinatortablayout;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 /**
@@ -13,15 +19,22 @@ import android.widget.ImageView;
  */
 
 public class Coordinatortablayout extends CoordinatorLayout {
+    private Context mContext;
+    private Toolbar mToolbar;
+    private ActionBar mActionbar;
+    private TabLayout mTabLayout;
     private ImageView mImageView;
-    private int mImageSrc;
+    private ViewPager mViewPager;
+    private int[] mImageArray;
 
     public Coordinatortablayout(Context context) {
         super(context);
+        mContext = context;
     }
 
     public Coordinatortablayout(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mContext = context;
         initView(context);
         initWidget(context, attrs);
 
@@ -29,23 +42,91 @@ public class Coordinatortablayout extends CoordinatorLayout {
 
     public Coordinatortablayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        mContext = context;
         initView(context);
         initWidget(context, attrs);
     }
 
     private void initView(Context context) {
         LayoutInflater.from(context).inflate(R.layout.view_coordinatortablayout, this, true);
-
-        mImageView = (ImageView) findViewById(R.id.imageview);
-
+        initToolbar();
+        setupTabLayout();
     }
 
     private void initWidget(Context context, AttributeSet attrs) {
         TypedArray typedArray = context.obtainStyledAttributes(attrs
                 , R.styleable.Coordinatortablayout);
-        mImageSrc = typedArray.getResourceId(R.styleable.Coordinatortablayout_tab_image_src, 0);
-        mImageView.setImageResource(mImageSrc);
+
     }
 
+    private void setupTabLayout() {
+        mTabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        mTabLayout.setupWithViewPager(mViewPager);
+        mImageView = (ImageView) findViewById(R.id.imageview);
+        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                mImageView.startAnimation(AnimationUtils.loadAnimation(mContext, R.anim.anim_dismiss));
+                mImageView.setImageResource(mImageArray[tab.getPosition()]);
+                mImageView.setAnimation(AnimationUtils.loadAnimation(mContext, R.anim.anim_show));
+            }
 
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+    }
+
+    private void initToolbar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        ((AppCompatActivity) mContext).setSupportActionBar(mToolbar);
+        mActionbar = ((AppCompatActivity) mContext).getSupportActionBar();
+    }
+
+    /**
+     * 设置Toolbar标题
+     *
+     * @param title 标题
+     */
+    public void setToolbarTitle(String title) {
+        if (mActionbar != null) {
+            mActionbar.setTitle(title);
+        }
+    }
+
+    /**
+     * 设置Toolbar显示返回按钮及标题
+     *
+     * @param canBack 是否返回
+     */
+    public void setToolbarBackEnable(Boolean canBack) {
+        if (canBack && mActionbar != null) {
+            mActionbar.setDisplayHomeAsUpEnabled(true);
+            mActionbar.setHomeAsUpIndicator(R.drawable.ic_arrow_white_24dp);
+        }
+    }
+
+    /**
+     * 设置与该组件搭配的ViewPager
+     *
+     * @param vp 与TabLayout结合的ViewPager
+     */
+    public void setupWithViewPager(ViewPager vp) {
+        mViewPager = vp;
+    }
+
+    /**
+     * 设置每个tab对应的图片
+     *
+     * @param imageArray 图片数组
+     */
+    public void setupImageArray(int[] imageArray) {
+        if (imageArray != null) {
+            mImageArray = imageArray;
+        }
+    }
 }
